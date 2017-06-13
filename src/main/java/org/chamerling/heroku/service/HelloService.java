dd04b379569d6693a015c461b745f2cf21bd4999
arinalistyarini <arinalistyarini@yahoo.com>
2017-06-13 11:58:54 +0700

    add webmethod webparam

--- src/main/java/org/chamerling/heroku/service/HelloService.java
+++ src/main/java/org/chamerling/heroku/service/HelloService.java
@@ -3,6 +3,8 @@
  */
 package org.chamerling.heroku.service;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 /**
@@ -13,5 +15,6 @@ import javax.jws.WebService;
 public interface HelloService {
 
 	String sayHi(String input);
-        Boolean ubahSaldo(int pil, String idKartu, int nominal, int saldoKartu, String via);
+        @WebMethod(operationName = "ubahSaldo")
+        public Boolean ubahSaldo(@WebParam(name = "pil") int pil, @WebParam(name = "idKartu") String idKartu, @WebParam(name = "nominal") int nominal, @WebParam(name = "saldoKartu") int saldoKartu, @WebParam(name = "via") String via);
 }
